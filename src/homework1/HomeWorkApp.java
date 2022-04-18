d1e967c3cfcd038e9d5658c30bf66dd3e99b4bd2
alekssan <sandakovaalexx@mail.ru>
2022-04-18 19:36:20 +0800

    My first homework

--- src/homework1/HomeWorkApp.java
+++ src/homework1/HomeWorkApp.java
@@ -1,6 +1,6 @@
 package homework1;
 
-  public class HomeWorkApp {
+public class HomeWorkApp {
     public static void main(String[] args){
         printThreeWords();
         checkSumSign();
@@ -8,69 +8,47 @@ package homework1;
         compareNumbers();
     }
 
-      public static void printThreeWords(){
-          System.out.println("Orange");
-          System.out.println("Banana");
-          System.out.println("Apple");
-      }
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
 
-      public static void checkSumSign(){
-          int a = 5;
-          int b = 7;
-          int c = a + b;
+    public static void checkSumSign(){
+        int a = 5;
+        int b = 7;
+        int c = a + b;
 
-          if (c >= 0) {
-              System.out.println("Сумма положительная");
-          } else {
-              System.out.println("Сумма отрицательная");
-          }
-      }
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
 
-      public static void printColor(){
-          int value = 101;
+    public static void printColor(){
+        int value = 101;
 
-          if (value <= 0) {
-              System.out.println("Красный");
-          }
-          if (value >= 1 && value <= 100){
-              System.out.println("Желтый");
-          }
-          if (value > 100){
-              System.out.println("Зеленый");
-          }
-      }
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value >= 1 && value <= 100){
+            System.out.println("Желтый");
+        }
+        if (value > 100){
+            System.out.println("Зеленый");
+        }
+    }
 
-      public static void compareNumbers(){
-          int a = 3;
-          int b = 3;
+    public static void compareNumbers(){
+        int a = 3;
+        int b = 3;
 
-          if (a >= b){
-              System.out.println("a >= b");
-          } else {
-              System.out.println("a < b");
-          }
-      }
+        if (a >= b){
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  }
+}
